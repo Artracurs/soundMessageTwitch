91c6d1f1c785391b698234990658ed15bee3fc93
Ruslan Nurgaleev <43127114+Artracurs@users.noreply.github.com>
2022-03-11 18:09:28 +0500

    play sound when new people write first time

--- bottwitch.js
+++ bottwitch.js
@@ -1,4 +1,3 @@
-
 const client = new tmi.Client({
 	channels: [ 'itnarog' ]
 });
@@ -6,6 +5,14 @@ const client = new tmi.Client({
 client.connect();
 
 client.on('message', (channel, tags, message, self) => {
-  $("#myAudioElement")[0].play();
 	console.log(`${tags['display-name']}: ${message}`);
+
+	const date = new Date()
+	const firstMsgDate = [date.getFullYear(), date.getMonth(), date.getDay()]
+
+	if (localStorage.getItem(`${tags['display-name']}`) === null ) {
+		$("#myaudio")[0].play();
+		localStorage.setItem(`${tags['display-name']}`, firstMsgDate.toString())
+	}
+
 });
