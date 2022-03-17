1be8b0931f103ae208379b1be51bcbd476f75e45
0xdf00ff@gmail.com <0xdf00ff@gmail.com>
2022-03-17 22:59:29 +0500

    om

--- src/Test/Test1.jsx
+++ src/Test/Test1.jsx
@@ -1,24 +1,66 @@
 import axios from 'axios';
 
-function axer() {
-  axios('https://www.myinstants.com/instant/talking-ben-yes-46136/', {
+const url1 = "https://jsonplaceholder.typicode.com/todos"
+const url2 = "https://www.myinstants.com/instant/talking-ben-yes-46136/"
+
+async function axer() {
+  let resp = await fetch(url2, {
+    method: 'GET',
+    mode: 'no-cors',
     headers: {
-      method: 'GET',
-      mode: 'no-cors',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      withCredentials: true,
-      credentials: 'same-origin',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Headers': 'X-Requested-With',
+      'Access-Control-Allow-Headers': 'Content-Type',
     },
+    credentials: 'same-origin',
+    referrerPolicy: 'no-referrer'
   })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
+ 
+
+  let content = await resp.text()
+
+  console.log(content)
+  // axios('http://www.myinstants.com/instant/talking-ben-yes-46136/', {
+  //   headers: {
+  //     method: 'GET',
+  //     mode: 'no-cors',
+  //     headers: {
+  //       'Access-Control-Allow-Origin': '*',
+  //       'Access-Control-Allow-Methods': 'GET, POST',
+  //       'Access-Control-Allow-Headers': 'X-Requested-With',
+  //       'Access-Control-Allow-Headers': 'text/html',
+  //     }
+  //   }
+  // })
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   })
+
+  // const testURL = 'https://dashboard.heroku.com/apps/twitch-sound-alerts/deploy/github';
+  // const myInit = {
+  //   method: 'GET',
+  //   mode: 'no-cors',
+  //   headers: {
+  //     'Access-Control-Allow-Origin': '*',
+  //     'Content-Type': 'application/json'
+  //   },
+  //   withCredentials: true,
+  //   credentials: 'same-origin',
+  // };
+
+  // const myRequest = new Request(testURL, myInit);
+
+  // fetch(myRequest).then(function (response) {
+  //   return response;
+  // }).then(function (response) {
+  //   console.log(response);
+  // }).catch(function (e) {
+  //   console.log(e);
+  // });
 }
 
 
