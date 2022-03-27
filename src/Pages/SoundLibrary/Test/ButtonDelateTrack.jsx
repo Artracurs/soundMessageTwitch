b9aa75f334f4a4f0637867d887e4b7e0fcdeb384
0xdf00ff@gmail.com <0xdf00ff@gmail.com>
2022-03-27 18:41:34 +0500

    ver 0.3

--- src/Pages/SoundLibrary/Test/ButtonDelateTrack.jsx
+++ src/Pages/SoundLibrary/Test/ButtonDelateTrack.jsx
@@ -13,31 +13,31 @@ export const PlayAudioFile = (props) => {
   const audio = new Audio(props.url)
   let [audioEnd, setaudioEnd] = useState(audio.ended)
 
-  
+
   const PlayByID = () => {
     audio.volume = volume
     audio.play()
-    document.getElementById(props.id).style.background = "#0d6dfd"
+    document.getElementById(props.url).style.background = "#0d6dfd"
   }
 
 
   const TrackDuration = () => {
-    document.getElementById(props.id).style.background = "#0a6dfd"
-    document.getElementById(props.id).innerHTML = `${audio.duration.toFixed(2)} | sec`
+    document.getElementById(props.url).style.background = "#0a6dfd"
+    document.getElementById(props.url).innerHTML = `${audio.duration.toFixed(2)} | sec`
   }
   const ResetDuration = () => {
-    document.getElementById(props.id).style.background = "rgba(128, 128, 128, 0.192)"
-    document.getElementById(props.id).innerHTML = "Play"
+    document.getElementById(props.url).style.background = "rgba(128, 128, 128, 0.192)"
+    document.getElementById(props.url).innerHTML = "Play"
   }
 
   useEffect(() => {
     audio.addEventListener('ended', setaudioEnd = () => {
-      document.getElementById(props.id).style.background = "rgba(128, 128, 128, 0.192)"
+      document.getElementById(props.url).style.background = "rgba(128, 128, 128, 0.192)"
     });
   })
 
   return (<div>
-    <button onMouseLeave={ResetDuration} onMouseEnter={TrackDuration} id={props.id} className={s.id} onClick={PlayByID} >Play</button>
+    <button onMouseLeave={ResetDuration} onMouseEnter={TrackDuration} id={props.url} className={s.id} onClick={PlayByID} >Play</button>
   </div>);
 }
 
@@ -45,13 +45,30 @@ export const PlayAudioFile = (props) => {
 
 
 
-
 const ButtonDelateTrack = (props) => {
+  let soundLibrary = localStorage.getItem("SoundLibrary")
+  let soundLibraryJSON = JSON.parse(soundLibrary)
+
+
+
+  const [slist, setSlist] = useState(soundLibraryJSON)
+
+
   const DeleteTrack = () => {
-    console.log("deleted")
+
+    const index = slist.findIndex(n => n.id === props.id);
+    if (index !== -1) {
+      soundLibraryJSON.splice(index, 1)
+    }
+
+    setSlist(()=>  {slist = 0;
+      
+      localStorage.setItem("SoundLibrary", JSON.stringify(soundLibraryJSON))})
+  
 
 
 
+    // window.location.reload();
   }
 
 
@@ -67,14 +84,14 @@ const ButtonDelateTrack = (props) => {
 
 
   const Selected = () => {
-    document.getElementById(props.url).style.background = "red"
+    document.getElementById(props.id).style.background = "red"
   }
   const Unselected = () => {
-    document.getElementById(props.url).style.background = "rgba(128, 128, 128, 0.192)"
+    document.getElementById(props.id).style.background = "rgba(128, 128, 128, 0.192)"
   }
 
   return (<div>
-    <button id={props.url} onMouseLeave={Unselected} onMouseEnter={Selected} onClick={DeleteTrack} className={s.id}>Delete{props.delete}</button>
+    <button id={props.id} onMouseLeave={Unselected} onMouseEnter={Selected} onClick={DeleteTrack} className={s.id}>Delete{props.delete}</button>
   </div>)
 
 }
